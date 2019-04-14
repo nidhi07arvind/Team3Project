d9336e541df8a01de67d72d90ceda65b1cd9ebb0
dg-sjsu <42924770+dg-sjsu@users.noreply.github.com>
2019-04-13 17:57:58 -0700

    Add files via upload
    players.php and display_players.php pages modified to use same style/design as index.html

--- display_players.php
+++ display_players.php
@@ -4,10 +4,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <meta charset="utf-8">
+  <title>Cricket World Cup 2019</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
 </head>
 
+
 <body>
+
+<div class="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid navbar-custom">
+
+       
+        <div class="row">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <div class="col-xs-9 phone-nav">
+              <a class="navbar-brand" href="#" id="logo">Home</a>
+            </div>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right navbar-right-custom">
+      
+              <li><a href="Put link to schedule page here">Schedule</a></li>
+              <li><a href="pointsTable.php">Points table</a></li>
+              <li><a href="Put link to Teams page here">Teams</a></li>
+              <li><a href="players.php">Players</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="hero text-center">
+      <h1>Cricket World Cup 2019</h1>
+      <p>The fun is Out There</p>
+    </div>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js"></script>
+  <script src="data.js"></script>
+  <script src="app.js"></script>
+</body> 
+
+
+
 <?php
   //foreach($_POST as $key=>$value)
   //{
@@ -15,26 +67,27 @@
   //}
 
   $player_table_name = 'Players';
+  $player_table_c1_name_spacer = '      |       ';
 
-  $player_c1_hdr = 'F_Name';
-  $player_c2_hdr = 'L_Name';
-  $player_c3_hdr = 'Player_Id';
-  $player_c4_hdr = 'Runs';
-  $player_c5_hdr = 'Tot_Num_Matches';
-  $player_c6_hdr = 'Balls';
-  $player_c7_hdr = 'Wickets';
-  $player_c8_hdr = 'Team_Id';
-  $player_c9_hdr = 'Specialization';
+  $player_table_c1_hdr = 'F_Name';
+  $player_table_c2_hdr = 'L_Name';
+  $player_table_c3_hdr = 'Player_Id';
+  $player_table_c4_hdr = 'Runs';
+  $player_table_c5_hdr = 'Tot_Num_Matches';
+  $player_table_c6_hdr = 'Balls';
+  $player_table_c7_hdr = 'Wickets';
+  $player_table_c8_hdr = 'Team_Id';
+  $player_table_c9_hdr = 'Specialization';
   
-  $player_c1_name = 'First name';
-  $player_c2_name = 'Last name';
-  $player_c3_name = 'Player ID';
-  $player_c4_name = 'Runs';
-  $player_c5_name = 'Total number of matches played';
-  $player_c6_name = 'Balls';
-  $player_c7_name = 'Wickets';
-  $player_c8_name = 'Team';
-  $player_c9_name = 'Specialization';
+  $player_table_c1_name = 'First name' . $player_table_c1_name_spacer;
+  $player_table_c2_name = 'Last name' . $player_table_c1_name_spacer;
+  $player_table_c3_name = 'Player ID' . $player_table_c1_name_spacer;
+  $player_table_c4_name = 'Runs' . $player_table_c1_name_spacer;
+  $player_table_c5_name = 'Total number of matches played' . $player_table_c1_name_spacer;
+  $player_table_c6_name = 'Balls' . $player_table_c1_name_spacer;
+  $player_table_c7_name = 'Wickets' . $player_table_c1_name_spacer;
+  $player_table_c8_name = 'Team name' . $player_table_c1_name_spacer;
+  $player_table_c9_name = 'Specialization' . $player_table_c1_name_spacer;
  
 
   $select_str = "";
@@ -44,91 +97,99 @@
 
   if (!empty($_POST['sTeam']))
   {
-    $where_str .=  $player_c8_hdr . '="' . $_POST['sTeam'] . '" AND ';
+    $where_str .=  $player_table_c8_hdr . '="' . $_POST['sTeam'] . '" AND ';
   }
-  $select_str .= $player_c8_hdr . ',';
-  $hdr_str .= "<td>$player_c8_name</td>";
+  $select_str .= $player_table_c8_hdr . ',';
+  $hdr_str .= "<td>$player_table_c8_name</td>";
 
   if (!empty($_POST['sFName']))
   {
-    $where_str .=  $player_c1_hdr . '="' . $_POST['sFName'] . '" AND ';
+    $where_str .=  $player_table_c1_hdr . '="' . $_POST['sFName'] . '" AND ';
   }
-  $select_str .= $player_c1_hdr . ',';
-  $hdr_str .= "<td>$player_c1_name</td>";
+  $select_str .= $player_table_c1_hdr . ',';
+  $hdr_str .= "<td>$player_table_c1_name</td>";
 
   if (!empty($_POST['sLName']))
   {
-    $where_str .=  $player_c2_hdr . '="' . $_POST['sLName'] . '" AND ';
+    $where_str .=  $player_table_c2_hdr . '="' . $_POST['sLName'] . '" AND ';
   }
-  $select_str .= $player_c2_hdr . ',';
-  $hdr_str .= "<td>$player_c2_name</td>";
+  $select_str .= $player_table_c2_hdr . ',';
+  $hdr_str .= "<td>$player_table_c2_name</td>";
 
   if (array_key_exists('sRuns',$_POST))
   {
-    $select_str .= $player_c4_hdr . ',';
-    $hdr_str .= "<td>$player_c4_name</td>";
+    $select_str .= $player_table_c4_hdr . ',';
+    $hdr_str .= "<td>$player_table_c4_name</td>";
     $numValues += 1;
   }
 
   if (array_key_exists('sBalls',$_POST))
   {
-    $select_str .= $player_c6_hdr . ',';
-    $hdr_str .= "<td>$player_c6_name</td>";
+    $select_str .= $player_table_c6_hdr . ',';
+    $hdr_str .= "<td>$player_table_c6_name</td>";
     $numValues += 1;
   }
 
   if (array_key_exists('sWickets',$_POST))
   {
-    $select_str .= $player_c7_hdr . ',';
-    $hdr_str .= "<td>$player_c7_name</td>";
+    $select_str .= $player_table_c7_hdr . ',';
+    $hdr_str .= "<td>$player_table_c7_name</td>";
     $numValues += 1;
   }
 
   if (array_key_exists('sNumMatches',$_POST))
   {
-    $select_str .= $player_c5_hdr . ',';
-    $hdr_str .= "<td>$player_c5_name</td>";
+    $select_str .= $player_table_c5_hdr . ',';
+    $hdr_str .= "<td>$player_table_c5_name</td>";
     $numValues += 1;
   }
 
   if (array_key_exists('sSpecialization',$_POST))
   {
-    $select_str .= $player_c9_hdr . ',';
-    $hdr_str .= "<td>$player_c9_name</td>";
+    $select_str .= $player_table_c9_hdr . ',';
+    $hdr_str .= "<td>$player_table_c9_name</td>";
     $numValues += 1;
   }
-
 
   $select_str = rtrim($select_str,',');
   $where_str = rtrim($where_str,' AND ');
   $hdr_str .= "</tr>";
 
   //echo $select_str;
-
-  $select_str0 = $select_str;
+  if ((empty($_POST['sTeam'])) && (empty($_POST['sFName'])) && (empty($_POST['sLName']))) 
+  {
+    $select_str0 = '*';
+    $where_str0 = '';
+  }
+  else
+  {
+    $select_str0 = $select_str;
+    $where_str0 = $where_str;
+  }
+  
   $table_str0 = 'Player';
-  $where_str0 = $where_str;
+
 
   mysqli_select_db($conn, $db);
 
   $sql = "SELECT $select_str0 FROM $table_str0 " . $where_str0 ;
-  echo $sql;
+  //echo $sql;
   $r = mysqli_query($conn, $sql) or die("Bad query: $sql");
 
 
   echo"<table border='1'>";
-  //echo"<tr><td>$player_c1_name</td><td>$player_c2_name</td><td>$player_c3_name</td><td>$player_c4_name</td><td>$player_c5_name</td><td>$player_c6_name</td><td>$player_c7_name</td><td>$player_c8_name</td><td>$player_c9_name</td></tr>\n";
+  //echo"<tr><td>$player_table_c1_name</td><td>$player_table_c2_name</td><td>$player_table_c3_name</td><td>$player_table_c4_name</td><td>$player_table_c5_name</td><td>$player_table_c6_name</td><td>$player_table_c7_name</td><td>$player_table_c8_name</td><td>$player_table_c9_name</td></tr>\n";
   echo $hdr_str;
   while($a = $r->fetch_assoc()){
       echo "<tr>";
-      echo "<td>{$a[$player_c8_hdr]}</td>";
-      echo "<td>{$a[$player_c1_hdr]}</td>";
-      echo "<td>{$a[$player_c2_hdr]}</td>";
-      if (!empty($_POST['sRuns'])) {echo "<td>{$a[$player_c4_hdr]}</td>";}
-      if (!empty($_POST['sBalls'])) {echo "<td>{$a[$player_c6_hdr]}</td>";}
-      if (!empty($_POST['sWickets'])) {echo "<td>{$a[$player_c7_hdr]}</td>";}
-      if (!empty($_POST['sNumMatches'])) {echo "<td>{$a[$player_c5_hdr]}</td>";}
-      if (!empty($_POST['sSpecialization'])) {echo "<td>{$a[$player_c9_hdr]}</td>";}
+      echo "<td>{$a[$player_table_c8_hdr]}</td>";
+      echo "<td>{$a[$player_table_c1_hdr]}</td>";
+      echo "<td>{$a[$player_table_c2_hdr]}</td>";
+      if (!empty($_POST['sRuns'])) {echo "<td>{$a[$player_table_c4_hdr]}</td>";}
+      if (!empty($_POST['sBalls'])) {echo "<td>{$a[$player_table_c6_hdr]}</td>";}
+      if (!empty($_POST['sWickets'])) {echo "<td>{$a[$player_table_c7_hdr]}</td>";}
+      if (!empty($_POST['sNumMatches'])) {echo "<td>{$a[$player_table_c5_hdr]}</td>";}
+      if (!empty($_POST['sSpecialization'])) {echo "<td>{$a[$player_table_c9_hdr]}</td>";}
       
       echo "</tr>";
     }
@@ -139,7 +200,8 @@
 
 ?>
 
-</body> 
+
+
 </html>
 
 
--- players.php
+++ players.php
@@ -5,10 +5,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <meta charset="utf-8">
+  <title>Cricket World Cup 2019</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
 </head>
 
+
 <body>
+
+<div class="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid navbar-custom">
+
+       
+        <div class="row">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <div class="col-xs-9 phone-nav">
+              <a class="navbar-brand" href="#" id="logo">Home</a>
+            </div>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right navbar-right-custom">
+      
+              <li><a href="Put link to schedule page here">Schedule</a></li>
+              <li><a href="pointsTable.php">Points table</a></li>
+              <li><a href="Put link to Teams page here">Teams</a></li>
+              <li><a href="players.php">Players</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="hero text-center">
+      <h1>Cricket World Cup 2019</h1>
+      <p>The fun is Out There</p>
+    </div>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js"></script>
+  <script src="data.js"></script>
+  <script src="app.js"></script>
+</body> 
+
 <?php
   $select_str0 = '*';
   $table_str0 = 'Player';
@@ -39,10 +89,10 @@
   $player_c5_name = 'Total number of matches played';
   $player_c6_name = 'Balls';
   $player_c7_name = 'Wickets';
-  $player_c8_name = 'Team';
+  $player_c8_name = 'Team name';
   $player_c9_name = 'Specialization';
   
-
+/*
   echo"<table border='1'>";
   //echo"<tr><td>$player_c1_name</td><td>$player_c2_name</td><td>$player_c3_name</td><td>$player_c4_name</td><td>$player_c5_name</td><td>$player_c6_name</td><td>$player_c7_name</td><td>$player_c8_name</td><td>$player_c9_name</td></tr>\n";
   echo"<tr><td>$player_c8_name</td><td>$player_c1_name</td><td>$player_c2_name</td></tr>\n";
@@ -61,15 +111,18 @@
       echo "</tr>";
     }
   echo "</table>";
+*/
 ?>
 
+Fetch player information
 <form action="display_players.php" method="post">
-  Team: <input type="text" name=sTeam ><br>
-  Player First name: <input type="text" name=sFName> and Last name: <input type="text" name=sLName> <br>
+  Team name         : <input type="text" name=sTeam ><br>
+  Player First name : <input type="text" name=sFName><br> 
+  Player Last name  : <input type="text" name=sLName> <br>
   <input type="checkbox" name=sRuns value=1> Runs<br>
   <input type="checkbox" name=sBalls value=1> Balls<br>  
   <input type="checkbox" name=sWickets value=1> Wickets<br> 
-  <input type="checkbox" name=sNumMatches value=1> Num. matched played<br> 
+  <input type="checkbox" name=sNumMatches value=1> # of matches played<br> 
   <input type="checkbox" name=sSpecialization value=1> Specialization<br> 
   <input type="submit" value="Submit">
 </form>
