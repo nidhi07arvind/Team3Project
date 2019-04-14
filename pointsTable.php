541fb4d3b7ebcc5d57f2dc2f98dde2ef388861a3
dg-sjsu <42924770+dg-sjsu@users.noreply.github.com>
2019-04-13 20:41:27 -0700

    Add files via upload
    Updated design and navbars on pointsTable.php

--- pointsTable.php
+++ pointsTable.php
@@ -4,10 +4,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <meta charset="utf-8">
+  <title>Cricket World Cup 2019</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
 </head>
 
+
 <body>
+
+<div class="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid navbar-custom">
+
+       
+        <div class="row">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <div class="col-xs-9 phone-nav">
+              <a class="navbar-brand" href="#" id="logo">Home</a>
+            </div>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right navbar-right-custom">
+      
+              <li><a href="schedule.php">Schedule</a></li>
+              <!---li><a href="pointsTable.php">Points table</a></li--->
+              <li><a href="teams.php">Teams</a></li>
+              <li><a href="players.php">Players</a></li> 
+              <li><a href="login.php">Login</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="hero text-center">
+      <h1>Cricket World Cup 2019</h1>
+      <p>The fun is Out There</p>
+    </div>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js"></script>
+  <script src="data.js"></script>
+  <script src="app.js"></script>
+</body> 
 <?php
     $sql = "SELECT * FROM PointsTable;";
     $result = mysqli_query($conn, $sql) or die("Bad query: $sql");
@@ -31,7 +81,7 @@
     echo "</table>";
   }
 ?>
-</body> 
+
 </html>
 
 
