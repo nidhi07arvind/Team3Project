681704aedd9620f16c6de681f74f8229010569ab
VineetKhatwal <48118060+VineetKhatwal@users.noreply.github.com>
2019-04-16 18:16:11 -0700

    Add files via upload

--- Schedule.php
+++ Schedule.php
@@ -1,12 +1,11 @@
 <?php
-  include_once 'includes/dbh.inc.php';
-  $_POST = array()
+  include_once 'includes/dbh.inc.php'
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Cricket World Cup 2019</title>
+  <title>Schedule Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
@@ -15,14 +14,10 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
 </head>
 
-
 <body>
-
 <div class="wrapper">
     <nav class="navbar navbar-default">
       <div class="container-fluid navbar-custom">
-
-       
         <div class="row">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -40,15 +35,42 @@
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right navbar-right-custom">
       
-              <li><a href="Put link to schedule page here">Schedule</a></li>
-              <li><a href="pointsTable.php">Points table</a></li>
-              <li><a href="Put link to Teams page here">Teams</a></li>
-              <li><a href="players.php">Players</a></li>
+              <li><a href="schedule.php">Points Table</a></li>
+              <li><a href="teams.php">Teams</a></li>
+              <li><a href="players.php">Players</a></li> 
+              <li><a href="login.php">Login</a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         </div>
       </div><!-- /.container-fluid -->
     </nav>
+    <?php
+		echo"<table border='1'>";
+  		$sql = "SELECT * FROM Matches";
+  		$result = mysqli_query($conn, $sql) or die("Bad query: $sql");
+        $resultcheck = mysqli_num_rows($result);
+        if ($resultcheck >0) {
+        	
+            echo "<tr>";
+            echo "<th id ='headerTable'><b>Match ID</b></th>";
+            echo "<th id ='headerTable'>Date&Time</th>";
+            echo "<th id ='headerTable'>Venue</th> ";
+            echo "<th id ='headerTable'>Team_Id</th>";
+            echo "<th id ='headerTable'>ManOfTheMatch</th>";
+            echo "</tr>";
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td id ='rowVal'>{$row['Match_Id']}</td>";
+                echo "<td id ='rowVal'>{$row['MatchDateAndTime']}</td>";
+                echo "<td id ='rowVal'>{$row['Venue']}</td>";
+                echo "<td id ='rowVal'>{$row['Team_Id']}</td>";
+                echo "<td id ='rowVal'>{$row['ManOfTheMatch']}</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+    	}
+
+		?>
     <div class="hero text-center">
       <h1>Cricket World Cup 2019</h1>
       <p>The fun is Out There</p>
@@ -59,32 +81,5 @@
   <script src="app.js"></script>
 </body> 
 
-<?php
-  $sql = "SELECT * FROM Matches;";
-  $result = mysqli_query($conn, $sql) or die("Bad query: $sql");
-  $resultcheck = mysqli_num_rows($result);
-  if ($resultcheck >0) {		
-  echo"<tr>
-    	<td id ="headerTable">Match ID</td>
-    	<td id ="headerTable">Date&Time</td>
-    	<td id ="headerTable">Venue</td>
-    	<td id ="headerTable">Team_Id</td>
-    	<td id ="headerTable">ManOfTheMatch</td>
-    	</tr>\n";
-    			
-    	while($row = mysqli_fetch_assoc($result)) {
-       		echo "<tr>";
-        	echo "<td id ='rowVal'>{$row['Match_Id']}</td>";
-        	echo "<td id ='rowVal'>{$row['MatchDateAndTime']}</td>";
-        	echo "<td id ='rowVal'>{$row['Venue']}</td>";
-        	echo "<td id ='rowVal'>{$row['Team_Id']}</td>";
-        	echo "<td id ='rowVal'>{$row['ManOfTheMatch']}</td>";
-        	echo "</tr>";
-     	}
-    	echo "</table>";
-    }
-  */
-?>
 
-</body> 
 </html>
